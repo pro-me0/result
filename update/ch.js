80b36e5f20a2b5e4e33f2a34f8a302a27723547d
proMe <secondarymazi@gmail.com>
2022-08-15 18:31:05 +0100

    Delete ch.js

--- update/ch.js
+++
@@ -1,7 +0,0 @@
-/* var one = document.getElementById('one');
-var two = document.getElementById('two');
- */
-let save = () => {
-    let cont = document.getElementById('text').value;
-    window.open("data:application/txt," + encodeURIComponent(cont), "_self");
-}
